69a532afec45b0298b4a9e10722d0c2dfaaf4a84
morbug <morbug@outlook.com>
2014-07-16 12:23:53 +0200

    Empty project

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("DiceRoller")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("HP")> 
+<Assembly: AssemblyProduct("DiceRoller")> 
+<Assembly: AssemblyCopyright("Copyright © HP 2014")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("b52f9ba5-8b3c-452f-a7da-e603ce4f700a")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
